df2653cd619f532e173b9d6f5b4a85b03b168085
Aravind M <darthvader@Qs-iMac-2.local>
2016-07-12 11:36:58 +0530

    Add test for 0 and 1 parameter Add required code to pass the test

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,3 +1,10 @@
 # string calculator
 class StringCalculator
+  def self.add(input)
+    if input.empty?
+      0
+    else
+      input.to_i
+    end
+  end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,5 +1,22 @@
 require 'string_calculator'
 # spec of string calculator
 describe StringCalculator do
+  describe ".add" do
+    context 'given an empty string' do
+      it 'should return zero' do
+        expect(StringCalculator.add('')).to eql(0)
+      end
+    end
 
+    context 'given 4' do
+      it 'should return 4' do
+        expect(StringCalculator.add('4')).to eql(4)
+      end
+    end
+    context 'given 10' do
+      it 'should return 10' do
+        expect(StringCalculator.add('10')).to eql(10)
+      end
+    end
+  end
 end
